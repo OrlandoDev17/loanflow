113e41597dbccabe27b1e95e6d0eeb56e10200a2
OrlandoDev17 <orlandodev1707@gmail.com>
2025-10-02 18:21:59 -0400

    Configurar donde se podra hacer peticiones

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,10 @@ const clientesRoutes = require("./routes/clientes");
 const app = express();
 app.use(
   cors({
-    origin: true,
-    // origin: [
-    //   "https://loanflow.vercel.app", // ✅ tu frontend en producción
-    //   "http://localhost:3000", // ✅ para desarrollo local
-    // ],
+    origin: [
+      "https://loanflow.vercel.app", // ✅ tu frontend en producción
+      "http://localhost:3000", // ✅ para desarrollo local
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
